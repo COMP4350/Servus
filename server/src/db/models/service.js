c3b1cd2d854312ea6835ac34ee9d6e6c1f34dd1f
Arvind Maan <arvindsmaan@gmail.com>
2021-02-24 19:41:50 -0600

    Add location data to services

--- server/src/db/models/service.js
+++ server/src/db/models/service.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const { Schema } = mongoose;
+const { Schema, Decimal128 } = mongoose;
 
 const serviceSchema = new Schema({
     provider: { type: String, required: true },
@@ -9,6 +9,11 @@ const serviceSchema = new Schema({
     cost: { type: String, required: true },
     duration: { type: String, required: false },
     availability: { type: String, required: true },
+    location: {
+        lat: { type: Decimal128, required: true },
+        lng: { type: Decimal128, required: true },
+        address: { type: String, required: true },
+    },
 });
 
 const Service = mongoose.model('Service', serviceSchema);
