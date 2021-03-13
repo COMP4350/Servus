c40cc24fb0e012cb9549bd15f1ec03fd18378d50
Arvind Maan <arvindsmaan@gmail.com>
2021-03-12 19:37:29 -0600

    Add appointment tests

--- server/src/routes/appointment.js
+++ server/src/routes/appointment.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Service = require('../db/models/service.js');
 const Appointment = require('../db/models/appointment.js');
+const User = require('../db/models/user.js');
 
 const router = express.Router();
 
@@ -18,12 +19,33 @@ router.get('/:username', (req, res) => {
     if (start_date) searchQuery.booked_time.$gte = new Date(start_date);
     if (end_date) searchQuery.booked_time.$lt = new Date(end_date);
 
-    Appointment.find(searchQuery).then(appointments => {
-        if (appointments) {
-            return res.status(200).json({ result: appointments });
-        }
-        return res.status(404).json([]);
-    });
+    User.findOne({ username: req.params.username })
+        .then(user => {
+            if (user) {
+                Appointment.find(searchQuery)
+                    .then(appointments => {
+                        if (appointments) {
+                            return res
+                                .status(200)
+                                .json({ result: appointments });
+                        } else {
+                            return res.status(404).json([]);
+                        }
+                    })
+                    .catch(err => {
+                        return res
+                            .status(500)
+                            .json({ errors: [{ error: err }] });
+                    });
+            } else {
+                return res
+                    .status(404)
+                    .json({ errors: [{ user: 'not found' }] });
+            }
+        })
+        .catch(err => {
+            return res.status(500).json({ errors: [{ error: err }] });
+        });
 });
 
 /* Create new appointment */
@@ -32,51 +54,56 @@ router.post('/:buyer', (req, res) => {
     let provider = '';
 
     Service.findById(req.body.service_id).then(service => {
-        if (service) provider = service.provider;
-        else
+        if (service) {
+            provider = service.provider;
+
+            Appointment.findOne({ ...req.body, buyer: req.params.buyer }).then(
+                appointment => {
+                    if (appointment)
+                        return res.status(422).json({
+                            errors: [
+                                { appointment: 'appointment already exists' },
+                            ],
+                        });
+                    else {
+                        const newAppointment = new Appointment({
+                            buyer: req.params.buyer,
+                            provider: provider,
+                            service_id: req.body.service_id,
+                            booked_time: req.body.booked_time,
+                            created_at: Date.now(),
+                        });
+                        newAppointment
+                            .save()
+                            .then(response => {
+                                return res.status(200).json({
+                                    success: true,
+                                    result: response,
+                                });
+                            })
+                            .catch(err => {
+                                return res.status(500).json({
+                                    errors: [{ error: err }],
+                                });
+                            });
+                    }
+                }
+            );
+        } else
             return res
                 .status(404)
                 .json({ error: 'service does not exist for appointment' });
     });
-
-    Appointment.findOne({ ...req.body, buyer: req.params.buyer }).then(
-        appointment => {
-            if (appointment)
-                return res.status(422).json({
-                    errors: [{ appointment: 'appointment already exists' }],
-                });
-            else {
-                const newAppointment = new Appointment({
-                    buyer: req.params.buyer,
-                    provider: provider,
-                    service_id: req.body.service_id,
-                    booked_time: req.body.booked_time,
-                    created_at: Date.now(),
-                });
-                newAppointment
-                    .save()
-                    .then(response => {
-                        return res.status(200).json({
-                            success: true,
-                            result: response,
-                        });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        return res.status(500).json({
-                            errors: [{ error: err }],
-                        });
-                    });
-            }
-        }
-    );
 });
 
 /* DELETE appointments(s). */
 router.delete('/:appointment_id', (req, res) => {
     Appointment.findByIdAndRemove(req.params.appointment_id, err => {
-        if (err) throw err;
-        res.status(200);
+        if (err) {
+            return res.status(500).json({ success: false });
+        } else {
+            return res.status(200).json({ success: true });
+        }
     });
 });
 
--- server/test/test_utils.js
+++ server/test/test_utils.js
@@ -21,6 +21,15 @@ const defaultServiceOptions = {
     availability: '123',
     location: { lat: 42, lng: 43, address: '123' },
 };
+const defaultServiceOptions2 = {
+    username: 'testuser2',
+    name: 'testservice',
+    description: '123',
+    cost: '123',
+    duration: '123',
+    availability: '123',
+    location: { lat: 42, lng: 43, address: '123' },
+};
 
 const createDummyUser = (
     options = defaultUserOptions,
@@ -63,8 +72,65 @@ const createDummyUserWithServices = (
     });
 };
 
+const createDummyUsersWithServicesAndAppointments = (
+    usernameOne = 'testuser',
+    usernameTwo = 'testuser2',
+    options = defaultUserOptions,
+    serviceOptions = defaultServiceOptions,
+    serviceOptions2 = defaultServiceOptions2,
+    booked_time1 = Date.now(),
+    booked_time2 = Date.now()
+) => {
+    return new Promise((resolve, reject) => {
+        createDummyUserWithServices(usernameOne, options, serviceOptions)
+            .then(serviceOne => {
+                //at this point we have one user with one service. lets make another with another service
+                createDummyUserWithServices(
+                    usernameTwo,
+                    options,
+                    serviceOptions2
+                )
+                    .then(serviceTwo => {
+                        //now we make appointsments
+                        chai.request(app)
+                            .post(`/appointment/${usernameOne}`)
+                            .send({
+                                service_id: serviceOne.body.result._id,
+                                booked_time: booked_time1,
+                            })
+                            .then(appointmentOne => {
+                                //now we create a service for this user
+                                chai.request(app)
+                                    .post(`/appointment/${usernameTwo}`)
+                                    .send({
+                                        service_id: serviceTwo.body.result._id,
+                                        booked_time: booked_time2,
+                                    })
+                                    .then(appointmentTwo => {
+                                        resolve({
+                                            appointmentOne:
+                                                appointmentOne.body.result,
+                                            appointmentTwo:
+                                                appointmentTwo.body.result,
+                                        });
+                                    })
+                                    .catch(error => {
+                                        reject(error);
+                                    });
+                            })
+                            .catch(err => reject(err));
+                    })
+                    .catch(err => reject(err));
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
 module.exports = {
     createDummyUser,
     createDummyUserWithServices,
+    createDummyUsersWithServicesAndAppointments,
     defaultServiceOptions,
 };
