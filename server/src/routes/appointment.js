828d894312a2e939a8fb5d5f9b6d8cc23922ad6c
Arvind Maan <arvindsmaan@gmail.com>
2021-03-21 22:21:29 -0500

    Consider conflicting appointments

--- server/src/routes/appointment.js
+++ server/src/routes/appointment.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Service = require('../db/models/service.js');
 const Appointment = require('../db/models/appointment.js');
 const User = require('../db/models/user.js');
+const moment = require('moment');
+const { duration } = require('moment');
 
 const router = express.Router();
 
@@ -36,6 +38,47 @@ const inTimeSlot = (service, request) => {
         }
     }
     return true;
+};
+
+const noConflicts = (service, request) => {
+    const duration_hours = service.duration.slice(0, 2);
+    const duration_minutes = service.duration.slice(2);
+    const desired_start_time = moment(new Date(request.body.booked_time));
+    const desired_end_time = moment(new Date(request.body.booked_time))
+        .add(duration_hours, 'h')
+        .add(duration_minutes, 'm');
+
+    return new Promise((resolve, reject) => {
+        Appointment.find({ service_id: service._id })
+            .then(appointments => {
+                for (let index in appointments) {
+                    let appointment = appointments[index];
+                    let apt_start_time = moment(
+                        new Date(appointment.booked_time)
+                    );
+                    let apt_end_time = moment(new Date(appointment.booked_time))
+                        .add(duration_hours, 'h')
+                        .add(duration_minutes, 'm');
+                    //check if the desired_time collides with the appointment timeslot
+                    if (
+                        apt_start_time <= desired_start_time &&
+                        apt_end_time >= desired_start_time
+                    ) {
+                        return resolve(false);
+                    }
+                    if (
+                        apt_start_time <= desired_end_time &&
+                        apt_end_time >= desired_end_time
+                    ) {
+                        return resolve(false);
+                    }
+                }
+                return resolve(true);
+            })
+            .catch(err => {
+                return reject(err);
+            });
+    });
 };
 /* Get appointments for username */
 router.get('/:username', (req, res) => {
@@ -129,38 +172,61 @@ router.post('/:buyer', (req, res) => {
                 });
             }
 
-            Appointment.findOne({ ...req.body, buyer: req.params.buyer }).then(
-                appointment => {
-                    if (appointment)
-                        return res.status(422).json({
+            noConflicts(service, req)
+                .then(nocons => {
+                    if (nocons) {
+                        Appointment.findOne({
+                            ...req.body,
+                            buyer: req.params.buyer,
+                        }).then(appointment => {
+                            if (appointment)
+                                return res.status(422).json({
+                                    errors: [
+                                        {
+                                            appointment:
+                                                'appointment already exists',
+                                        },
+                                    ],
+                                });
+                            else {
+                                const newAppointment = new Appointment({
+                                    buyer: req.params.buyer,
+                                    provider: provider,
+                                    service_id: req.body.service_id,
+                                    booked_time: req.body.booked_time,
+                                    created_at: Date.now(),
+                                });
+                                newAppointment
+                                    .save()
+                                    .then(response => {
+                                        return res.status(200).json({
+                                            success: true,
+                                            result: response,
+                                        });
+                                    })
+                                    .catch(err => {
+                                        return res.status(500).json({
+                                            errors: [{ error: err }],
+                                        });
+                                    });
+                            }
+                        });
+                    } else {
+                        return res.status(500).json({
                             errors: [
-                                { appointment: 'appointment already exists' },
+                                {
+                                    error:
+                                        'appointment time invalid – conflicts with other appointments',
+                                },
                             ],
                         });
-                    else {
-                        const newAppointment = new Appointment({
-                            buyer: req.params.buyer,
-                            provider: provider,
-                            service_id: req.body.service_id,
-                            booked_time: req.body.booked_time,
-                            created_at: Date.now(),
-                        });
-                        newAppointment
-                            .save()
-                            .then(response => {
-                                return res.status(200).json({
-                                    success: true,
-                                    result: response,
-                                });
-                            })
-                            .catch(err => {
-                                return res.status(500).json({
-                                    errors: [{ error: err }],
-                                });
-                            });
                     }
-                }
-            );
+                })
+                .catch(err => {
+                    return res.status(500).json({
+                        errors: [{ error: err }],
+                    });
+                });
         } else
             return res
                 .status(404)
