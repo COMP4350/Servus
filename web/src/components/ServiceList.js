4d41a0a0a16c4356d3fadc609972e3e03ce00976
Caden Chabot <chabotc@myumanitoba.ca>
2021-03-24 02:21:36 -0500

    created search bar

--- web/src/components/ServiceList.js
+++ web/src/components/ServiceList.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core';
 import ServiceCard from './ServiceCard';
+import Search from '@material-ui/icons/Search';
+import { TextField, InputAdornment } from '@material-ui/core';
+import useForm from '../hooks/useForm';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -9,10 +12,17 @@ const useStyles = makeStyles(() => ({
         marginLeft: 10,
         marginTop: 10,
     },
+    searchBar: {
+        marginBottom: '10px',
+    },
+    searchIcon: {
+        cursor: 'pointer',
+    },
 }));
 
 const ServiceList = () => {
     const [services, setServices] = useState(null);
+    const [searchForm, onSearchFormChange] = useForm({ search: '' });
     const classes = useStyles();
     const getServices = async () => {
         const response = await axios.get(`/services/`);
@@ -21,10 +31,31 @@ const ServiceList = () => {
     useEffect(() => {
         getServices();
     }, []);
-
+    const search = () => {
+        console.log(`searching ${searchForm.search}`);
+    };
     return (
         <div>
             <div className={classes.root}>
+                <TextField
+                    className={classes.serchBar}
+                    id="input-with-icon-textfield"
+                    label="Search"
+                    name="search"
+                    onChange={onSearchFormChange}
+                    value={searchForm.search}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment
+                                className={classes.searchIcon}
+                                position="end"
+                                onClick={search}>
+                                <Search />
+                            </InputAdornment>
+                        ),
+                    }}
+                    onKeyDown={e => (e.key === 'Enter' ? search : null)}
+                />
                 {services
                     ? services.map((service, index) => {
                           return (
