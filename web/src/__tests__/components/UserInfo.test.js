d2b7fac1f7fe0ec52d9b3f0c7e7d615e191e4b9a
Andy Tan <tana@myumanitoba.ca>
2021-03-25 11:55:50 -0500

    Add another component test for UserInfo

---
+++ web/src/__tests__/components/UserInfo.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import UserInfo from '../../components/UserInfo';
+
+describe('UserInfo', () => {
+    it('renders correctly', async () => {
+        const { asFragment } = render(
+            <UserInfo/>
+        );
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
