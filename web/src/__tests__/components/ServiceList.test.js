3d6be25e5464d1d1f0446fd82f4f87688447ba0a
Andy Tan <tana@myumanitoba.ca>
2021-04-03 02:27:15 -0500

    Fixed ServiceList tests to account for sidebar changes

--- web/src/__tests__/components/ServiceList.test.js
+++ web/src/__tests__/components/ServiceList.test.js
@@ -3,6 +3,28 @@ import { act, render } from '@testing-library/react';
 import ServiceList from '../../components/ServiceList';
 import { mockServiceList } from '../../__mocks__/getMockServiceList';
 import mockAxios from 'axios';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#647AA3',
+        },
+        secondary: {
+            main: '#EC5732',
+        },
+        default: {
+            main: '#FFF2EB',
+        },
+    },
+    background: {
+        dark: '#151515',
+        main: '#272727',
+    },
+    typographs: {
+        fontFamily: ['Roboto'],
+    },
+});
 
 describe('ServiceList', () => {
     it('renders correctly', async () => {
@@ -16,11 +38,13 @@ describe('ServiceList', () => {
         );
 
         const { asFragment } = render(
-            <ServiceList setServices={handleServiceUpdate} />
+            <ThemeProvider theme={theme}>
+                <ServiceList setServices={handleServiceUpdate} />
+            </ThemeProvider>
         );
         expect(asFragment()).toMatchSnapshot();
-        expect(mockAxios.get).toHaveBeenCalledWith('/services/');
-        expect(mockAxios.get).toHaveBeenCalledTimes(1);
+        expect(mockAxios.post).toHaveBeenCalledWith('/services/filter', {"tags": []});
+        expect(mockAxios.post).toHaveBeenCalledTimes(1);
 
         await act(() => promise);
     });
