b9ecc325f66e86d464c23769e31da867a08ab95a
Caden Chabot <chabotc@myumanitoba.ca>
2021-04-08 15:42:46 -0500

    Cc/setup cypress (#148)
    * installed cypress
    
    * created stub tests
    
    * added cypress script
    
    Co-authored-by: Arvind Maan <arvindsmaan@gmail.com>

---
+++ web/cypress/integration/BookAppointments.spec.js
@@ -0,0 +1,5 @@
+describe('book appointment', () => {
+    it('visits the page', () => {
+        cy.visit('http://localhost:3000/');
+    });
+});
---
+++ web/cypress/integration/FindServices.spec.js
@@ -0,0 +1,5 @@
+describe('find service on map', () => {
+    it('visits the page', () => {
+        cy.visit('http://localhost:3000/');
+    });
+});
