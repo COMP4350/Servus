faaaecc124f64bf8b5a2b0dd165cb7664f7fcbb2
Caden Chabot <chabotc@myumanitoba.ca>
2021-04-08 15:05:32 -0500

    Update FindServices.spec.js

--- web/cypress/integration/FindServices.spec.js
+++ web/cypress/integration/FindServices.spec.js
@@ -2,10 +2,4 @@ describe('find service on map', () => {
     it('visits the page', () => {
         cy.visit('http://localhost:3000/');
     });
-    it('enters location', () => {
-        cy.get('.makeStyles-addressContainer-10').type('Winnipeg');
-        cy.get('#root').click();
-        cy.get('#search-address').click();
-    });
-    it('finds service', () => {});
 });
