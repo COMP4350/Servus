46d3f9744f0c9defde25ceee725564015fc7526c
Andy Tan <tana@myumanitoba.ca>
2021-04-09 04:56:14 -0500

    Prototype test for advertising past work.

--- web/cypress/integration/AdvertisePastWork.spec.js
+++ web/cypress/integration/AdvertisePastWork.spec.js
@@ -1,5 +1,61 @@
 describe('advertise past work', () => {
-    it('visits the page', () => {
+    it('Registers a test account.', () => {
         cy.visit('http://localhost:3000/');
+        cy.clearCookies();
+
+        // Fill in a new user registration form to create a test user.
+        cy.get('[data-cy=signup]').click();
+
+        cy.get('[data-cy=signup_first_name]')
+            .type('TestFirstName')
+            .should('have.value', 'TestFirstName');
+
+        cy.get('[data-cy=signup_last_name]')
+            .type('TestLastName')
+            .should('have.value', 'TestLastName');
+
+        cy.get('[data-cy=signup_username]')
+            .type('TestUsername')
+            .should('have.value', 'TestUsername');
+
+        cy.get('[data-cy=signup_password]')
+            .type('TestPassword')
+            .should('have.value', 'TestPassword');
+
+        cy.get('[data-cy=signup_confirm_password]')
+            .type('TestPassword')
+            .should('have.value', 'TestPassword');
+
+            
+        cy.get('[data-cy=create_account_button]').click();
+        cy.wait(1500);
+    })
+
+    // Note: until this bug is fixed, this login section is necessary.
+    // Logging in should not be necessary when an account is created,
+    // since Servus automatically logs into newly created accounts.
+
+    it('Logs into the test account.', () => {
+        cy.get('[data-cy=header_username]').click();
+
+        cy.get('[data-cy=username]')
+            .type('TestUsername')
+            .should('have.value', 'TestUsername');
+        cy.get('[data-cy=password]')
+            .type('TestPassword')
+            .should('have.value', 'TestPassword');
+        // Click login
+        cy.get('[data-cy=login]').click();
     });
+
+
+    it('Upload an image from the user profile.', () => {
+        cy.wait(1500);
+        // Move to the user profile.
+        cy.get('[data-cy=header_username]').click();
+
+        cy.wait(1500);
+
+        // Test uploading an imageboard picture.
+    })
 });
